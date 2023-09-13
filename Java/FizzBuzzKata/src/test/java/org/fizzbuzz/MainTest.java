a0ba720babe2ad85b551f73ced7ae924784ffa0f
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2023-09-13 10:54:30 +0200

    Test_the_truth

---
+++ Java/FizzBuzzKata/src/test/java/org/fizzbuzz/MainTest.java
@@ -0,0 +1,12 @@
+package org.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+     void Test_the_truth() {
+        assertTrue(true);
+    }
+}
